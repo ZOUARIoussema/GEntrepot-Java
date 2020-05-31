17050119acc578e65ca2942984b66d06ac8ecd10
Nadia Cheikh <60850329+nadiacheikh@users.noreply.github.com>
2020-05-31 22:46:48 +0200

    verif ajouter fournisseur
    verification par matricule fiscale

--- GEntrepot/src/com/gentrepot/services/ServiceFournisseur.java
+++ GEntrepot/src/com/gentrepot/services/ServiceFournisseur.java
@@ -13,39 +13,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.control.Alert;
 
 /**
  *
  * @author oussema
  */
-public class ServiceFournisseur implements IService<Fournisseur>{
-        
-        
-    
-     Connection cnx = DataSource.getInstance().getCnx();
+public class ServiceFournisseur implements IService<Fournisseur> {
+
+    Connection cnx = DataSource.getInstance().getCnx();
 
     @Override
     public void ajouter(Fournisseur t) {
-        try {
-            String requete = "INSERT INTO fournisseur (raisonSociale,numeroTelephone,adresse,adresseMail,matriculeFiscale,codePostale) VALUES (?,?,?,?,?,?)";
-            PreparedStatement pst = cnx.prepareStatement(requete);
-            pst.setString(1, t.getRaisonSociale());
-            pst.setInt(2, t.getNumeroTelephone());
-            pst.setString(3, t.getAdresse());
-            pst.setString(4, t.getAdresseMail());
-            pst.setString(5, t.getMatriculeFiscale());
-            pst.setInt(6, t.getCodePostale());
-            pst.executeUpdate();
-            System.out.println("Fournisseur ajouté !");
 
-        } catch (SQLException ex) {
-            System.err.println(ex.getMessage());
+        if (!this.verifParMatricule(t.getMatriculeFiscale())) {
+
+            try {
+                String requete = "INSERT INTO fournisseur (raisonSociale,numeroTelephone,adresse,adresseMail,matriculeFiscale,codePostale) VALUES (?,?,?,?,?,?)";
+                PreparedStatement pst = cnx.prepareStatement(requete);
+                pst.setString(1, t.getRaisonSociale());
+                pst.setInt(2, t.getNumeroTelephone());
+                pst.setString(3, t.getAdresse());
+                pst.setString(4, t.getAdresseMail());
+                pst.setString(5, t.getMatriculeFiscale());
+                pst.setInt(6, t.getCodePostale());
+                pst.executeUpdate();
+                System.out.println("Fournisseur ajouté !");
+
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Ajouter Fournisseur");
+            alert.setHeaderText("matricule fiscale deja existe !");
+
+            alert.showAndWait();
+
         }
     }
 
     @Override
     public void supprimer(Fournisseur t) {
-         try {
+        try {
             String requete = "DELETE FROM personne WHERE id=?";
             PreparedStatement pst = cnx.prepareStatement(requete);
             pst.setInt(1, t.getId());
@@ -59,7 +70,7 @@ public class ServiceFournisseur implements IService<Fournisseur>{
 
     @Override
     public void modifier(Fournisseur t) {
-       try {
+        try {
             String requete = "UPDATE fournisseur SET raisonSociale=?,numeroTelephone=?,adresse=?,adresseMail=?,matriculeFiscale=?,codePostale=? WHERE id=?";
             PreparedStatement pst = cnx.prepareStatement(requete);
             pst.setInt(7, t.getId());
@@ -74,7 +85,7 @@ public class ServiceFournisseur implements IService<Fournisseur>{
 
         } catch (SQLException ex) {
             System.err.println(ex.getMessage());
-        } 
+        }
     }
 
     @Override
@@ -95,21 +106,24 @@ public class ServiceFournisseur implements IService<Fournisseur>{
 
         return list;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-      public Fournisseur rechercher(List<Fournisseur> et, int in){
+
+    public boolean verifParMatricule(String mt) {
+
+        for (Fournisseur f : this.afficher()) {
+
+            if (f.getMatriculeFiscale().equals(mt)) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public Fournisseur rechercher(List<Fournisseur> et, int in) {
         int a = 0;
-        for(int i=0;i<et.size();i++){
-            if(et.get(i).getId() == in){
+        for (int i = 0; i < et.size(); i++) {
+            if (et.get(i).getId() == in) {
                 a = i;
             }
         }
